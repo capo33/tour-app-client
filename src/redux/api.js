74eee0b59f362034e0d139a68016ab171c76b174
Mohamed Mahmoud <76108008+capo33@users.noreply.github.com>
2023-01-30 16:10:29 +0200

    Update baseURL

--- src/redux/api.js
+++ src/redux/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+//   baseURL: "http://localhost:5000",
+  baseURL: "https://mern-tour-app-api.onrender.com",
+  
 });
 
 API.interceptors.request.use((req) => {
